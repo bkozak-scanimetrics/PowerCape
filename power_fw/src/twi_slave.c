8606b03af7e254154b651483ee527300520dc4bd
Billy Kozak <bkozak@scanimetrics.com>
2016-01-20 16:11:23 -0700

    add definitions for twi status codes
    At present, these codes are not very descriptive, names should be
    improved if possible at some point.

--- power_fw/src/twi_slave.c
+++ power_fw/src/twi_slave.c
@@ -14,6 +14,56 @@
 static volatile uint8_t data_count;
 static volatile uint8_t reg_index;
 /******************************************************************************
+*                         SLAVE RECIEVER STATUS CODES                         *
+******************************************************************************/
+/* Own SLA+W has been received; ACK has been returned */
+#define TWI_S_RCVR_STATE1 0X60
+/* Arbitration lost in SLA+R/W as Master; own SLA+W has been received; ACK has
+   been returned */
+#define TWI_S_RCVR_STATE2 0x68
+/* General call address has been received; ACK has been returned */
+#define TWI_S_RCVR_STATE3 0x70
+/* Arbitration lost in SLA+R/W as Master; General call address has been
+   received; ACK has been returned */
+#define TWI_S_RCVR_STATE4 0x78
+/* Previously addressed with own SLA+W; data has been received;
+   ACK has been returned */
+#define TWI_S_RCVR_STATE5 0x80
+/* Previously addressed with own SLA+W; data has been received; NOT ACK has
+   been returned */
+#define TWI_S_RCVR_STATE6 0x88
+/* Previously addressed with general call; data has been re- ceived;
+   ACK has been returned  */
+#define TWI_S_RCVR_STATE7 0x90
+/*  Previously addressed with general call; data has been received; NOT ACK
+   has been returned */
+#define TWI_S_RCVR_STATE8 0x98
+/* A STOP condition or repeated START condition has been received while still
+   addressed as Slave */
+#define TWI_S_RCVR_STATE9 0xA0
+/******************************************************************************
+*                       SLAVE TRANSMITTER STATUS CODES                        *
+******************************************************************************/
+/* Own SLA+R has been received; ACK has been returned */
+#define TWI_S_TRMR_STATE1 0xA8
+/* Arbitration lost in SLA+R/W as Master; own SLA+R has been received; ACK
+   has been returned */
+#define TWI_S_TRMR_STATE2 0xB0
+/* Data byte in TWDR has been transmitted; ACK has been received */
+#define TWI_S_TRMR_STATE3 0xB8
+/* Data byte in TWDR has been transmitted; NOT ACK has been received */
+#define TWI_S_TRMR_STATE4 0xC0
+/* Last data byte in TWDR has been transmitted (TWEA = “0”); ACK has been
+   received */
+#define TWI_S_TRMR_STATE5 0xC8
+/******************************************************************************
+*                              MISC STATUS CODES                              *
+******************************************************************************/
+/* No relevant state information available; TWINT = “0 */
+#define TWI_IDLE      0xF8
+/* Bus error due to an illegal START or STOP condition */
+#define TWI_BUS_ERROR 0x00
+/******************************************************************************
 *                            FUNCTION DEFINITIONS                             *
 ******************************************************************************/
 ISR( TWI_vect )
@@ -23,15 +73,15 @@ ISR( TWI_vect )
 
 	switch ( status )
 	{
-		case 0x60:  // SLA+W
-		case 0xA0:  // Stop or repeated start
+		case TWI_S_RCVR_STATE1:  // SLA+W
+		case TWI_S_RCVR_STATE9:  // Stop or repeated start
 		{
 			data_count = 0;
 			break;
 		}
 
-		case 0xA8:  // SLA+R
-		case 0xB8:  // Data sent + ACK
+		case TWI_S_TRMR_STATE1:  // SLA+R
+		case TWI_S_TRMR_STATE3:  // Data sent + ACK
 		{
 			TWDR = registers_host_read( reg_index++ );
 
@@ -43,7 +93,7 @@ ISR( TWI_vect )
 			break;
 		}
 
-		case 0x80:  // data RX
+		case TWI_S_RCVR_STATE5:  // data RX
 		{
 			data = TWDR;
 			TWCR |= ( 1 << TWINT );
@@ -66,12 +116,11 @@ ISR( TWI_vect )
 			return;
 		}
 
-		case 0xC0:  // data TX + NAK
-		case 0xC8:  // last data (TWEA=0) + ACK
+		case TWI_S_TRMR_STATE4:  // data TX + NAK
+		case TWI_S_TRMR_STATE5:  // last data (TWEA=0) + ACK
 		{
 			break;
 		}
-
 		default:
 		{
 			break;
