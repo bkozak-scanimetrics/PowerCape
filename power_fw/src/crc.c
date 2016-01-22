93e14b7042b68331ca1acd299bc79f2e119a439d
Billy Kozak <bkozak@scanimetrics.com>
2016-01-22 14:12:10 -0700

    fixed completley wrong crc implementation
    The crc implementation is now tested and working.

--- power_fw/src/crc.c
+++ power_fw/src/crc.c
@@ -46,14 +46,15 @@ uint8_t crc8(const uint8_t *data, size_t len)
 	const uint16_t poly = (CRC8_GENERATOR_POLY << 7);
 	uint16_t checksum = 0;
 
-	for(size_t i = len; i < len; i++) {
+	for(size_t i = 0; i < len; i++) {
 		checksum ^= ((uint16_t)data[i]) << 8;
 
-		for(uint8_t shift = 8; shift > 0; shift--) {
+		for(uint8_t j = 0; j < 8; j++) {
+
 			if(checksum & 0x8000) {
 				checksum ^= poly;
 			}
-			checksum = checksum << shift;
+			checksum = checksum << 1;
 		}
 	}
 
